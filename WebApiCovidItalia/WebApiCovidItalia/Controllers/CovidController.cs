92fed229dac7b0ede1b4435556f884546642d5b5
Damiano Amadori <amadori.damiano@gmail.com>
2020-11-12 23:23:53 +0100

    Controller refinement

--- WebApiCovidItalia/WebApiCovidItalia/Controllers/CovidController.cs
+++ WebApiCovidItalia/WebApiCovidItalia/Controllers/CovidController.cs
@@ -2,59 +2,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace WebApiCovidItalia.Controllers
 {
-	public class CovidItem
-	{
-		public DateTime data { get; set; }
-		public string stato { get; set; }
-		public int codice_regione { get; set; }
-		public string denominazione_regione { get; set; }
+    public class CovidItem
+    {
+        public DateTime data { get; set; }
+        public string stato { get; set; }
+        public int? codice_regione { get; set; }
+        public string denominazione_regione { get; set; }
+        public int? ricoverati_con_sintomi { get; set; }
+        public int? terapia_intensiva { get; set; }
+        public int? totale_ospedalizzati { get; set; }
+        public int? isolamento_domiciliare { get; set; }
+        public int? totale_positivi { get; set; }
+        public int? variazione_totale_positivi { get; set; }
+        public int? nuovi_positivi { get; set; }
+        public int? dimessi_guariti { get; set; }
+        public int? deceduti { get; set; }
+        public double? casi_da_sospetto_diagnostico { get; set; }
+        public double? casi_da_screening { get; set; }
+        public int? totale_casi { get; set; }
+        public int? tamponi { get; set; }
+        public double? casi_testati { get; set; }
+        public double? diff_positivi_tamponi { get; set; }
+    }
 
-		public int nuovi_positivi { get; set; }
-		public int tamponi { get; set; }
+    [ApiController]
+    [Route("[controller]")]
+    public class CovidController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<CovidItem> Get()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(
+                    "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json");
 
-		public double diff_positivi_tamponi { get; set; }
-	}
-	
-	[ApiController]
-	[Route("[controller]")]
-	public class CovidController: ControllerBase
-	{
-		
-		[HttpGet]
-		public IEnumerable<CovidItem> Get()
-		{
-			
-			using (WebClient webClient = new WebClient())
-			{
-				var json = webClient.DownloadString(
-					"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json");
-				//Console.WriteLine(json);
-				var covidItems = JsonConvert.DeserializeObject<List<CovidItem>>(json);
+                var covidItems = JsonConvert.DeserializeObject<List<CovidItem>>(json);
 
-				var tuscany = covidItems.Where(ci => ci.codice_regione == 9).ToList();
+                var tuscany = covidItems.Where(ci => ci.codice_regione == 9).ToList();
 
-				Console.WriteLine("Len: " + tuscany.Count);
+                Console.WriteLine("Len: " + tuscany.Count);
 
-				for (int i = 1; i < tuscany.Count; i++)
-				{
-					if (tuscany[i].tamponi - tuscany[i - 1].tamponi != 0)
-						tuscany[i].diff_positivi_tamponi =
-							(double) tuscany[i].nuovi_positivi / ((double)tuscany[i].tamponi - tuscany[i - 1].tamponi);
-					else
-						tuscany[i].diff_positivi_tamponi = -1;
+                for (int i = 1; i < tuscany.Count; i++)
+                {
+                    if (tuscany[i].tamponi - tuscany[i - 1].tamponi != 0)
+                        tuscany[i].diff_positivi_tamponi = (double) tuscany[i].nuovi_positivi / ((double) tuscany[i].tamponi - tuscany[i - 1].tamponi);
+                    else
+                        tuscany[i].diff_positivi_tamponi = -1;
 
-					Console.WriteLine(tuscany[i].data + " | " + tuscany[i].diff_positivi_tamponi*100 +" %");
-				}
+                    Console.WriteLine(tuscany[i].data + " | " + tuscany[i].diff_positivi_tamponi * 100 + " %");
+                }
 
-				return tuscany;
-			}
-		}
-	}
+                return tuscany;
+            }
+        }
+    }
 }
--- WebApiCovidItalia/WebApiCovidItalia/Startup.cs
+++ WebApiCovidItalia/WebApiCovidItalia/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApiCovidItalia
 {
@@ -26,6 +19,11 @@ namespace WebApiCovidItalia
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +36,9 @@ namespace WebApiCovidItalia
 
 			app.UseHttpsRedirection();
 
-			app.UseRouting();
+            app.UseRouting();
+
+			app.UseCors(options => options.AllowAnyOrigin());
 
 			app.UseAuthorization();
 
